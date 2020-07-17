256b9bdeddde76a2c99f632b12e30325df941553
seungju <kingsky32@gmail.com>
2020-07-18 07:48:50 +0900

    Modified Tabheader and TabIcons

---
+++ componetns/NavIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+import styles from "../styles";
+
+const NavIcon = ({ name, color = styles.blackColor, size = 26 }) =>
+  <Ionicons name={name} color={color} size={size} />;
+
+NavIcon.PropTypes = {
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.num
+};
+
+export default NavIcon;
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image, Platform } from "react-native";
 import { View } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import Home from "../screens/Tabs/Home";
@@ -7,6 +8,7 @@ import Notifications from "../screens/Tabs/Notifications";
 import Profile from "../screens/Tabs/Profile";
 import { createStackNavigator } from "react-navigation-stack";
 import MessagesLink from "../componetns/MessagesLink";
+import NavIcon from "../componetns/NavIcon";
 
 const stackFactory = (initialRoute, customConfig) =>
   createStackNavigator({
@@ -16,32 +18,75 @@ const stackFactory = (initialRoute, customConfig) =>
     }
   });
 
-export default createBottomTabNavigator({
-  Home: {
-    screen: stackFactory(Home, {
-      title: "Home",
-      headerRight: () => <MessagesLink />
-    })
-  },
-  Search: {
-    screen: stackFactory(Search, {
-      title: "Search"
-    })
-  },
-  Add: {
-    screen: () => <View />,
-    navigationOptions: {
-      tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+export default createBottomTabNavigator(
+  {
+    Home: {
+      screen: stackFactory(Home, {
+        headerRight: () => <MessagesLink />,
+        headerTitle: () =>
+          <Image
+            style={{ height: 28 }}
+            resizeMode={"contain"}
+            source={require("../assets/logo.png")}
+          />
+      }),
+      navigationOptions: {
+        tabBarIcon: <NavIcon name={Platform.OS === "ios" ? "ios-home" : "md-home"} />
+      }
+    },
+    Search: {
+      screen: stackFactory(Search, {
+        headerRight: () => <MessagesLink />,
+        headerTitle: () =>
+          <Image
+            style={{ height: 28 }}
+            resizeMode={"contain"}
+            source={require("../assets/logo.png")}
+          />
+      }),
+      navigationOptions: {
+        tabBarIcon: <NavIcon name={Platform.OS === "ios" ? "ios-search" : "md-search"} />
+      }
+    },
+    Add: {
+      screen: () => <View />,
+      navigationOptions: {
+        tabBarIcon: <NavIcon name={Platform.OS === "ios" ? "ios-add" : "md-add"} />,
+        tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+      }
+    },
+    Notifications: {
+      screen: stackFactory(Notifications, {
+        headerRight: () => <MessagesLink />,
+        headerTitle: () =>
+          <Image
+            style={{ height: 28 }}
+            resizeMode={"contain"}
+            source={require("../assets/logo.png")}
+          />
+      }),
+      navigationOptions: {
+        tabBarIcon: <NavIcon name={Platform.OS === "ios" ? "ios-heart" : "md-heart"} />
+      }
+    },
+    Profile: {
+      screen: stackFactory(Profile, {
+        headerRight: () => <MessagesLink />,
+        headerTitle: () =>
+          <Image
+            style={{ height: 28 }}
+            resizeMode={"contain"}
+            source={require("../assets/logo.png")}
+          />
+      }),
+      navigationOptions: {
+        tabBarIcon: <NavIcon name={Platform.OS === "ios" ? "ios-person" : "md-person"} />
+      }
     }
   },
-  Notifications: {
-    screen: stackFactory(Notifications, {
-      title: "Notifications"
-    })
-  },
-  Profile: {
-    screen: stackFactory(Profile, {
-      title: "Profile"
-    })
+  {
+    tabBarOptions: {
+      showLabel: false
+    }
   }
-});
+);
